da086659d7b6583f10f17ee432232d823b639c94
Erickinhou <willianerick17@gmail.com>
2021-12-15 01:18:13 -0300

    Add routes to the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import GlobalStyle from "./GlobalStyle";
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      {/* Switch stops as soon as whe find the first match  */}
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </>
   );
 }
