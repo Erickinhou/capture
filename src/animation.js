98039952166b712984861be8ed6a1abcc08f18ff
Erickinhou <willianerick17@gmail.com>
2021-12-16 11:21:26 -0300

    Add line animation

--- src/animation.js
+++ src/animation.js
@@ -14,7 +14,6 @@ export const pageAnimation = {
   },
   exit: {
     opacity: 0,
-    y: 300,
     transition: {
       duration: 0.5,
     },
@@ -43,7 +42,7 @@ export const titleAnimation = {
 
 export const photoAnimation = {
   hidden: {
-    scale: 1.2,
+    scale: 1.5,
     opacity: 0,
   },
   show: {
@@ -52,6 +51,40 @@ export const photoAnimation = {
     transition: {
       ease: "easeOut",
       duration: 0.75,
+    },
+  },
+};
+
+export const lineAnimation = {
+  hidden: { width: "0%" },
+  show: {
+    width: "100%",
+    transition: { duration: 1 },
+  },
+};
+
+export const slider = {
+  hidden: {
+    x: "-130%",
+    skew: "45deg",
+  },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: {
+      ease: "easeOut",
+      duration: 1,
+    },
+  },
+};
+
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+      ease: "easeOut",
     },
   },
 };
